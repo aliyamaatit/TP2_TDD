8211371f22189c71f4fb9246916d7c3aec27d6ae
Aliya Maatit <aliya1maatit@gmail.com>
2025-10-20 18:01:14 +0200

    test(crypto): SUCCESS crypt(message, pas) -> décalage + suffixe

--- tests/test_crypto.py
+++ tests/test_crypto.py
@@ -3,7 +3,12 @@ from crypto import crypt
 
 class TestCrypto(unittest.TestCase):
     def test_crypt_pas_1_sans_suffixe(self):
+        # A) crypt(message)
         self.assertEqual(crypt("aZ9!"), 'b! "')
+
+    def test_crypt_avec_pas_et_suffixe(self):
+        # B) crypt(message, pas)
+        self.assertEqual(crypt("abc", 3), "def3")
 
 if __name__ == "__main__":
     unittest.main()
