4642a785aacbd5b2ddfca938c0da6256405e7887
Aliya Maatit <aliya1maatit@gmail.com>
2025-10-20 17:41:53 +0200

    test: FAIL affiche(n1, n2) -> concat [n1..n2]

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -10,3 +10,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_affiche_jusqua_n(self):
         attendu = "12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee"
         self.assertEqual(affiche(15), attendu)
+
+    def test_affiche_intervalle_5_10(self):
+        self.assertEqual(affiche(5, 10), "BuzzFizz78FizzBuzz")
+
+    def test_affiche_intervalle_10_16(self):
+        self.assertEqual(affiche(10, 16), "Buzz11Fizz1314FrisBee16")
